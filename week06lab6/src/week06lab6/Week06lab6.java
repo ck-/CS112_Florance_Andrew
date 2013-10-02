c518d3fbd7c296328adb9500025445421c246511
Andrew Florance <peacefullpeach@gmail.com>
2013-10-01 21:56:18 -0700

    week06lab6

---
+++ week06lab6/src/week06lab6/Week06lab6.java
@@ -0,0 +1,105 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week06lab6;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Andrew
+ */
+public class Week06lab6 {
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         int play;
+         int winning= 0;
+         int losing= 0;
+         int i = 0;
+         int done;
+           Scanner userInput = new Scanner( System.in );
+      System.out.println("how many times do you want to play?");
+                play = userInput.nextInt();
+                done = (play/2) + 1;
+         do
+         {
+              
+         // Randomly Generate a weapon for the computer.    
+       
+         Random rand = new Random(); 
+         rand.nextInt(3); 
+         int ranWeapon = rand.nextInt(3) + 1;
+    
+             
+                i++;
+               
+        //Ask user for a guess.
+   System.out.println("Welcome to the rock paper Scissors game! Guess 1 for rock 2 for paper and 3 scissors");
+       int weapon = userInput.nextInt();
+       
+       //If player chooses rock
+      
+       if (weapon==1){
+           if (ranWeapon==3)
+               System.out.println("You Win.");
+           else if(ranWeapon==2)
+               System.out.println("You Lose");
+           else if (ranWeapon==1)
+                   System.out.println("Tied!");
+           else
+           System.out.println("Invalid input please try again.");
+       }
+       
+       
+       //If player chooses paper.
+        if (weapon==2){
+           if (ranWeapon==1){
+               System.out.println("You Win.");
+               winning= winning + 1;
+           }
+           else if(ranWeapon==3)
+               System.out.println("You Lose");
+           else if(ranWeapon==2)
+                   System.out.println("Tied!");
+           else
+           System.out.println("Invalid input please try again.");
+       }
+        
+        //If player chooses scissors.
+         if (weapon==3){
+           if (ranWeapon==2){
+               System.out.println("You Win.");
+                winning= winning + 1;
+           }
+           
+           else if(ranWeapon==1){
+               System.out.println("You Lose");
+               losing = losing + 1;
+           }
+           else if(ranWeapon==3)
+                   System.out.println("Tied!");
+           else
+           System.out.println("Invalid input please try again.");
+       }
+         
+        if (winning>done)
+            break;
+        if (losing>done)
+            break;
+         }
+       while(play>=i++);
+         
+  if (winning>losing)
+            System.out.println("You're the winner!");
+        else if (losing>winning)
+            System.out.println("You lost!");
+        else 
+            System.out.println("You tied!");
+    }
+}
