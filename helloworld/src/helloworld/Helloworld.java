7169799af32812ce398e5d82c9484c1a52a0d187
Andrew <peacefullpeach@gmail.com>
2013-10-24 01:03:24 -0700

    s

--- helloworld/src/helloworld/Helloworld.java
+++ helloworld/src/helloworld/Helloworld.java
@@ -15,7 +15,7 @@ public class Helloworld {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.print("Hello");
+        System.out.print("Ho");
         // TODO code application logic here
     }
 }
